ed28e1c5c3dd2b1bb521fadf83e9d013f6d0b9ce
Vitalina-Oleksiienko <vitalina_oleksiienko@yahoo.com>
2022-12-21 15:07:49 +0200

    add PP desktop

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -1,25 +1,11 @@
-$(function() {
-  // Set
-  var main = $('div.mm-dropdown .textfirst')
-  var li = $('div.mm-dropdown > ul > li.input-option')
-  //var inputoption = $("div.mm-dropdown .option")
-  // var default_text ='<img src="../src/images/Flag.svg" width="17" height="11"  alt=""><div class="input-option-txt ">+380</div><img src = "https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-arrow-down-b-128.png" width = "10" height = "10" class="down" /> ';
-
-  // Animation
-  main.click(function() {
-    main.html(main);
-    li.toggle('fast');
-  });
-
-  // Insert Data
-  li.click(function() {
-    // hide
-    li.toggle('fast');
-    var livalue = $(this).data('value');
-    var lihtml = $(this).html();
-    main.html(lihtml);
-    //inputoption.val(livalue);
-  });
-});
+$(".default_option").click(function(){
+    $(this).parent().toggleClass("active");
+})
+  
+$(".select_ul li").click(function(){
+    var currentel = $(this).html();
+    $(".default_option li").html(currentel);
+    $(this).parents(".select_wrap").removeClass("active");
+})
 
 
